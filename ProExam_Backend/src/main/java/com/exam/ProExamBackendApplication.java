1e7374217974f0f16151ff1eb46bc49b648df5e3
yadavmohan8600@gmail.com <yadavmohan8600@gmail.com>
2024-11-11 20:11:09 +0530

    e

--- ProExam_Backend/src/main/java/com/exam/ProExamBackendApplication.java
+++ ProExam_Backend/src/main/java/com/exam/ProExamBackendApplication.java
@@ -8,6 +8,8 @@ public class ProExamBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProExamBackendApplication.class, args);
+		
+		//Push on GitHub
 	}
 
 }
